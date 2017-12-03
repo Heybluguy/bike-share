8e8ed196c5a4ed66f6ec4b421b1ed02e190feeb7
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-03 13:43:25 -0700

    duration test passes

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -47,8 +47,13 @@ RSpec.describe Trip do
   end
 
   describe "Class Mehtods" do
-    it "can find average duration of rides"
 
+    it "can find average duration of rides" do
+      Trip.create(duration: 40, start_date: "29/8/2013 18:54", start_station_name: "Golden Gate at Polk", end_date: "29/8/2013 18:56", end_station_name: "Golden Gate at Polk", bike_id: 527, subscription: "Subscriber", zip_code: 94109)
+      Trip.create(duration: 109, start_date: "29/8/2013 13:25", start_station_name: "Santa Clara at Almaden", end_date: "29/8/2013 13:27", end_station_name: "Adobe on Almaden", bike_id: 679, subscription: "Subscriber", zip_code: 95112)
+      Trip.create(duration: 138, start_date: "29/8/2013 16:57", start_station_name: "Post at Kearney", end_date: "29/8/2013 16:59", end_station_name: "Post at Kearney", bike_id: 408, subscription: "Subscriber", zip_code: 94117)
 
+    expect(Trip.average_duration).to eql(95)
+    end
   end
 end
